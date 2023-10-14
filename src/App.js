aa0384a0e388bfe6b0ca7a1d165d2855e79382f3
Arham Shahyd <arhamshahyd@Arhams-MacBook-Pro.local>
2023-10-15 01:36:30 +0500

    init project

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Section from './components/Section'
+import Product from './components/Product'
+
+// ABIs
+import Shoponetoics from './abis/Shoponetoics.json'
+
+// Config
+import config from './config.json'
+
+function App() {
+  const [provider, setProvider] = useState(null)
+  const [shoponetoics, setShoponetoics] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const [electronics, setElectronics] = useState(null)
+  const [clothing, setClothing] = useState(null)
+  const [toys, setToys] = useState(null)
+
+  const [item, setItem] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const togglePop = (item) => {
+    setItem(item)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    const network = await provider.getNetwork()
+
+    const shoponetoics = new ethers.Contract(config[network.chainId].shoponetoics.address, Shoponetoics, provider)
+    setShoponetoics(shoponetoics)
+
+    const items = []
+
+    for (var i = 0; i < 9; i++) {
+      const item = await shoponetoics.items(i + 1)
+      items.push(item)
+    }
+
+    const electronics = items.filter((item) => item.category === 'electronics')
+    const clothing = items.filter((item) => item.category === 'clothing')
+    const toys = items.filter((item) => item.category === 'toys')
+
+    setElectronics(electronics)
+    setClothing(clothing)
+    setToys(toys)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  return (
+    <div>
+      <Navigation account={account} setAccount={setAccount} />
+
+      <h2>Shoponetoics Best Sellers</h2>
+
+      {electronics && clothing && toys && (
+        <>
+          <Section title={"Clothing & Jewelry"} items={clothing} togglePop={togglePop} />
+          <Section title={"Electronics & Gadgets"} items={electronics} togglePop={togglePop} />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+
+      {toggle && (
+        <Product item={item} provider={provider} account={account} shoponetoics={shoponetoics} togglePop={togglePop} />
+      )}
+    </div>
+  );
+}
+
+export default App;
